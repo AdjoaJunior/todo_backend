38bca45bd6fe8b00ad2be76c62fd59bd0bd4715f
AdjoaJunior <estagyemang7804@gmail.com>
2021-12-13 23:29:19 +0000

    no change

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 mongoose.connect(db,
     { useUnifiedTopology: true,
-        useNewUrlParser:true 
+       // useNewUrlParser:true 
     }).then(() => {
          console.log('Connected to MongoDB successfully')
          }) .catch((err)=>{
