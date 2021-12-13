2b97fb01c3dad5342859ba5e4313499132f066e8
AdjoaJunior <estagyemang7804@gmail.com>
2021-12-13 15:59:55 +0000

    port number changed

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 const db = process.env.DB_URL;
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(db,
     { useUnifiedTopology: true 
